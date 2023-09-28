e3779b4f7c80b6d1d500f8631f84896f960b948b
97Fangz <briansegem25@gmail.com>
2023-09-28 09:57:42 -0700

    ES6_promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,8 +1,23 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
-}
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName),
+    uploadPhoto(fileName)];
 
-export default handleProfileSignup;
+  return Promise.allSettled(promises)
+    .then((values) => {
+      const res = [];
+      values.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          res.push(value);
+        } else {
+          res.push({
+            status: value.status,
+            value: `Error: ${value.reason.message}`,
+          });
+        }
+      });
+      return res;
+    });
+}
